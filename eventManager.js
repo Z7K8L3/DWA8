6b1d5fd8352975a773d6daac75320d66ffdcdca6
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-10-25 23:19:23 +0200

    created eventManager.js and scripts.js

---
+++ eventManager.js
@@ -0,0 +1,23 @@
+class EventManager {
+    constructor() {
+      this.listeners = {};
+    }
+  
+    addEventListener(element, event, callback) {
+      if (!this.listeners[element]) {
+        this.listeners[element] = {};
+      }
+      this.listeners[element][event] = callback;
+      element.addEventListener(event, callback);
+    }
+  
+    removeEventListener(element, event) {
+      if (this.listeners[element] && this.listeners[element][event]) {
+        element.removeEventListener(event, this.listeners[element][event]);
+        delete this.listeners[element][event];
+      }
+    }
+  }
+  
+  export default EventManager;
+  
